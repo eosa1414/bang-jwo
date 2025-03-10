9ff3392bad4e3edcffeade92b7d9346e9f6fc54d
DESKTOP-4QCTMKG\SSAFY <kbj7565@gmail.com>
2025-03-11 01:39:21 +0900

    feat 프로젝트 기보코드 세팅

---
+++ back-end/src/main/java/com/bangjwo/BangjwoApplication.java
@@ -0,0 +1,13 @@
+package com.bangjwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangjwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangjwoApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/bangjwo/BangjwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangjwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangjwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
