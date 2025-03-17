0a93f7d251b734a786c44a85fcdb8433bbe205c3
DESKTOP-4QCTMKG\SSAFY <kbj7565@gmail.com>
2025-03-17 17:18:26 +0900

    fix: docker-file 수정

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-set -e  # 스크립트 실행 중 오류 발생 시 즉시 종료
 
-echo "Waiting for MySQL to be up..."
-wait-for-it.sh db:3306 --timeout=30 --strict -- echo "MySQL is up!"
-
-echo "Starting the application..."
-exec java -jar app.jar
+echo "Entrypoint script executed!"
