bff46133e2401ff099f638309abd17ff09847ea7
하정수 <hajs0808@naver.com>
2025-03-18 16:57:00 +0900

    feat: contract 배포

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,12 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.28",
+    solidity: "0.8.20",
+    networks: {
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+        },
+    },
 };
