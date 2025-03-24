6648dd4ea2ef8509540f3c53cb399d41054f9499
백지민 <eosa1414@gmail.com>
2025-03-24 10:37:23 +0900

    feat: footer 구현

--- front-end/src/components/footers/FooterDefault.tsx
+++ front-end/src/components/footers/FooterDefault.tsx
@@ -1,5 +1,9 @@
 const FooterDefault = () => {
-  return <footer>푸터</footer>;
+  return (
+    <footer className="bg-neutral-dark200 text-neutral-white p-[10px_12px]">
+      Copyrightⓒ 2025 HaeJwo All rights reserved.
+    </footer>
+  );
 };
 
 export default FooterDefault;
