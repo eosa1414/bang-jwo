21db25576867c4a59233ef892b91ca8d42509d30
InJun2 <os2864@naver.com>
2025-03-25 13:17:04 +0900

    fix: webclient - swagger 의존성 버전 충돌로 swagger 버전 변경

--- back-end/src/main/java/com/bangjwo/global/config/web/WebClientConfig.java
+++ back-end/src/main/java/com/bangjwo/global/config/web/WebClientConfig.java
@@ -1,7 +1,13 @@
 package com.bangjwo.global.config.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+	@Bean
+	public WebClient.Builder webClientBuilder() {
+		return WebClient.builder();
+	}
 }
