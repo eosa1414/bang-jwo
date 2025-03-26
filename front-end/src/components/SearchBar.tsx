39a33ad98b7872dca9258899a468fa2364aa1a71
백지민 <eosa1414@gmail.com>
2025-03-26 09:44:23 +0900

    feat: SearchBar의 max-width 수정

--- front-end/src/components/SearchBar.tsx
+++ front-end/src/components/SearchBar.tsx
@@ -12,7 +12,7 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="flex w-full max-w-xs border border-neutral-light100 rounded-full overflow-hidden p-[4px_8px_4px_14px]">
+    <div className="flex w-full max-w-sm border border-neutral-light100 rounded-full overflow-hidden p-[4px_8px_4px_14px]">
       <input
         type="text"
         value={searchQuery}
