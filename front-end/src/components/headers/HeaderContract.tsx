311c731d68dd34cb5c95cffb8f41abee3ee69505
Hyeonji Kang <11qtrm@gmail.com>
2025-03-28 02:54:28 +0900

    feat: 임대인 계약서

--- front-end/src/components/headers/HeaderContract.tsx
+++ front-end/src/components/headers/HeaderContract.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
   title?: string;
 }
 
-const Header = ({ title }: HeaderProps) => {
+const HeaderContract = ({ title }: HeaderProps) => {
   return (
     <header className="flex w-full h-[55px] px-[14px] justify-between items-center border-b border-neutral-light100">
       <Link to="/" className="flex items-center gap-2">
@@ -13,9 +13,7 @@ const Header = ({ title }: HeaderProps) => {
         <span className="text-lg font-['TmonMonsori'] text-gold">방줘</span>
       </Link>
 
-      <div className="text-base font-bold">
-        {title || "텍스트"}
-      </div>
+      <div className="text-base font-bold">{title || "텍스트"}</div>
 
       <div className="flex items-center mr-4">
         <i className="material-symbols-rounded text-xl">chat</i>
@@ -24,4 +22,4 @@ const Header = ({ title }: HeaderProps) => {
   );
 };
 
-export default Header;
+export default HeaderContract;
