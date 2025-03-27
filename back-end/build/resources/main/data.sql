9803d5f819e8eaab8ce0bb4c3c6845b8de2bc427
DESKTOP-4QCTMKG\SSAFY <kbj7565@gmail.com>
2025-03-27 16:26:44 +0900

    fix: conflict 해결

---
+++ back-end/build/resources/main/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO MEMBER (member_id, kakao_id, name, birthday, phone, profile_url, created_at, updated_at, deleted_at)
+SELECT 1, 123456789, '홍길동', '1995-07-15', '010-1234-5678',
+       'https://example2.com',
+       NOW(), NOW(), NULL
+    WHERE NOT EXISTS (SELECT 1 FROM MEMBER);
+
+INSERT INTO MEMBER (member_id, kakao_id, name, birthday, phone, profile_url, created_at, updated_at, deleted_at)
+SELECT 2, 987654321, '김철수', '1998-03-22', '010-5678-1234',
+       'https://example1.com',
+       NOW(), NOW(), NULL
+    WHERE NOT EXISTS (SELECT 1 FROM MEMBER WHERE member_id = 2);
