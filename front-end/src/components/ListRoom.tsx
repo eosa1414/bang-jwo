3a43b8e1222047762baa43b3c6cb63800ddbecfe
백지민 <eosa1414@gmail.com>
2025-03-28 10:29:53 +0900

    feat: ListItem을 선택하여 Detail 창을 열 수 있도록 구현, 기본적인 방 Detail 창의 레이아웃 구현(아직은 임시 문자열로 동일한 내용이 표시됨)

--- front-end/src/components/ListRoom.tsx
+++ front-end/src/components/ListRoom.tsx
@@ -4,13 +4,22 @@ import { Room } from "../types/roomTypes";
 interface ListRoomProps {
   rooms: Room[];
   listClassName?: string;
+  onSelectRoom: (roomId: number) => void;
 }
 
-const ListRoom = ({ rooms, listClassName = "" }: ListRoomProps) => {
+const ListRoom = ({
+  rooms,
+  listClassName = "",
+  onSelectRoom,
+}: ListRoomProps) => {
   return (
     <div className={`scroll-custom ${listClassName}`}>
       {rooms.map((room) => (
-        <ListItemRoom key={room.roomId} room={room} />
+        <ListItemRoom
+          key={room.roomId}
+          room={room}
+          onSelectRoom={onSelectRoom}
+        />
       ))}
     </div>
   );
