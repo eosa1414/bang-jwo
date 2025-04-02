80af3af21b9497f4416703c68800f2422694c110
InJun2 <os2864@naver.com>
2025-04-02 16:57:54 +0900

    fix: webClientConfig 수정

--- back-end/src/main/java/com/bangjwo/global/config/web/WebClientConfig.java
+++ back-end/src/main/java/com/bangjwo/global/config/web/WebClientConfig.java
@@ -2,12 +2,17 @@ package com.bangjwo.global.config.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 	@Bean
-	public WebClient.Builder webClientBuilder() {
-		return WebClient.builder();
+	public WebClient webClient() {
+		return WebClient.builder()
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+			.build();
 	}
+
 }
