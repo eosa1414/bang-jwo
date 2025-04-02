93698961d181aa51ad583455622df26e1ac9d66c
백지민 <eosa1414@gmail.com>
2025-04-02 14:03:25 +0900

    feat: 로그인 회원가입 화면 구현

--- front-end/src/features/auth/pages/PageLogin.tsx
+++ front-end/src/features/auth/pages/PageLogin.tsx
@@ -1,3 +1,5 @@
+import LineBox from "../../../components/LineBox";
+import Logo from "/logo.svg";
 import Buttonkakao from "../components/ButtonKakao";
 
 const PageLogin = () => {
@@ -12,8 +14,22 @@ const PageLogin = () => {
   };
 
   return (
-    <div>
-      <Buttonkakao onClick={handleLoginClick} />
+    <div className="flex flex-col items-center py-[4rem] px-[1rem]">
+      <LineBox addClassName="flex flex-col gap-8 py-[4.25rem] px-[3.25rem] max-w-full">
+        {/* logo */}
+        <div className="flex flex-col gap-4 items-center">
+          <img src={Logo} className="w-[200px]" alt="BangJwo logo" />
+          <p className="text-6xl font-['TmonMonsori'] text-gold">방줘</p>
+        </div>
+        {/* bottom */}
+        <div className="flex flex-col gap-6">
+          <div className="text-center text-lg">
+            <p>간편하게 로그인하고</p>
+            <p className="font-bold">수수료 없이 방을 거래해 보세요!</p>
+          </div>
+          <Buttonkakao onClick={handleLoginClick} />
+        </div>
+      </LineBox>
     </div>
   );
 };
