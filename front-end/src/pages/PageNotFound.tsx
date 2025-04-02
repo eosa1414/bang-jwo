f268bec75f8b6d00b1ffa319bd2fe5b3c030604e
백지민 <eosa1414@gmail.com>
2025-04-02 15:43:33 +0900

    feat: neutral-dark300 색상 추가 및 적용

--- front-end/src/pages/PageNotFound.tsx
+++ front-end/src/pages/PageNotFound.tsx
@@ -10,22 +10,22 @@ const PageNotFound = () => {
   };
 
   return (
-    <div className="w-full flex-grow flex flex-col text-center items-center">
+    <div className="w-full flex-grow flex flex-col text-center items-center text-neutral-dark300">
       {/* 404 message (PC) */}
       <div className="w-full overflow-hidden">
-        <p className="flex invisible sm:visible font-['TmonMonsori'] text-[calc(100vw/14)] text-neutral-black whitespace-nowrap min-h-[20vh] justify-center items-center">
+        <p className="flex invisible sm:visible font-['TmonMonsori'] text-[calc(100vw/14)] whitespace-nowrap min-h-[20vh] justify-center items-center">
           {Array(7).fill("404").join(" ")}
         </p>
-        <div className="divider black w-full w2" />
+        <div className="divider dark300 w-full w2" />
       </div>
 
       {/* content */}
       <div className="flex flex-col flex-grow gap-10 items-center justify-center">
         <div className="flex flex-col gap-2  items-center justify-center">
-          <p className="sm:hidden font-['TmonMonsori'] text-9xl text-neutral-black whitespace-nowrap">
+          <p className="sm:hidden font-['TmonMonsori'] text-9xl whitespace-nowrap">
             404
           </p>
-          <p className="text-sm text-neutral-black">Page Not Found</p>
+          <p className="text-sm">Page Not Found</p>
         </div>
         <div className="flex flex-col gap-2 items-center justify-center">
           <p className="font-bold text-xl flex items-center justify-center gap-1 mr-1">
@@ -41,7 +41,7 @@ const PageNotFound = () => {
 
       {/* bottom line */}
       <div className="w-full overflow-hidden">
-        <div className="divider black w-full w2" />
+        <div className="divider dark300 w-full w2" />
         <p className="min-h-[20vh]"></p>
       </div>
     </div>
