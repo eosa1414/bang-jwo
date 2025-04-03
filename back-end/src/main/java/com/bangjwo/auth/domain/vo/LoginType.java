11551958d686b0789ef2601c8a7335a890eb6c2c
InJun2 <os2864@naver.com>
2025-04-03 16:29:48 +0900

    docs: 스웨거 추가

--- back-end/src/main/java/com/bangjwo/auth/domain/vo/LoginType.java
+++ back-end/src/main/java/com/bangjwo/auth/domain/vo/LoginType.java
@@ -1,12 +1,18 @@
 package com.bangjwo.auth.domain.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "기존 유저 로그인인지, 신규 가입인지 구분을 위한 로그인 타입")
 public enum LoginType {
+
+	@Schema(description = "기존 회원 로그인")
 	LOGIN("로그인 성공"),
+
+	@Schema(description = "신규 회원 가입")
 	SIGNUP("회원가입 성공");
 
 	private final String description;
