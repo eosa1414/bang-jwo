b919624b27cb01d3bc2be597b196ca4dfa394604
InJun2 <os2864@naver.com>
2025-04-03 16:29:12 +0900

    fix: 스웨거 문서에 커스텀 어노테이션 hidden 설정으로 변경

--- back-end/src/main/java/com/bangjwo/global/config/SwaggerConfig.java
+++ back-end/src/main/java/com/bangjwo/global/config/SwaggerConfig.java
@@ -1,19 +1,29 @@
 package com.bangjwo.global.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
 @Configuration
+@SecurityScheme(
+	name = "JWT",
+	type = SecuritySchemeType.HTTP,
+	scheme = "bearer",
+	bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI springBangJwoOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("BangJwo API")
-                        .description("BangJwo service application")
-                        .version("v0.0.1")
-                        .license(new License().name("SSAFY BangJwo").url("https://lab.ssafy.com/s12-blochain-transaction-sub1/S12P21A404")));
-    }
+	@Bean
+	public OpenAPI springBangJwoOpenAPI() {
+		return new OpenAPI()
+			.info(new Info().title("BangJwo API")
+				.description("BangJwo service application")
+				.version("v0.0.1")
+				.license(new License().name("SSAFY BangJwo")
+					.url("https://lab.ssafy.com/s12-blochain-transaction-sub1/S12P21A404")));
+	}
 }
